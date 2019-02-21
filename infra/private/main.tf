eea1ab5ba90747200ae52a964db586f07b3b0f63
Rafal Jeczalik <rjeczalik@gmail.com>
2019-02-21 21:26:46 +0100

    infra: add scylla clusters

---
+++ infra/private/main.tf
@@ -0,0 +1,9 @@
+module "scylla-cluster" {
+	source  = "github.com/rjeczalik/terraform-aws-scylla"
+
+	aws_instance_type = "i3.large"
+
+	cluster_count = 3
+	cluster_user_cidr = ["0.0.0.0/0"]
+	cluster_broadcast = "private"
+}
